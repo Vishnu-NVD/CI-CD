1db530d8fa5823a1d7ee07f4705a9adb028177ab
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 09:00:58 +0530

    Update dat_row.tf

--- Terraform/Data Table/dat_row.tf
+++ Terraform/Data Table/dat_row.tf
@@ -1,10 +1,10 @@
 resource "genesyscloud_architect_datatable_row" "john-smith" {
-  datatable_id = genesyscloud_architect_datatable.customers.id
+  datatable_id = genesyscloud_architect_datatable.customers_1.id
   key_value    = "johnsmith@example.com"
   properties_json = jsonencode({
-    "identifier" = 2749
-    "address"    = "123 Main Street"
-    "vip"        = true
+    "id" = 2749
+    "First"    = "John"
+    "Last"        = "Smith"
   })
 }
 
