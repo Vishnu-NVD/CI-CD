16833ba66d5d23c6ae58cb51024379d1aad49bfa
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 11:48:50 +0530

    Create user.tf

---
+++ Terraform/User/user.tf
@@ -0,0 +1,66 @@
+resource "genesyscloud_user" "example_user" {
+  email           = "johnny@servion.com"
+  name            = "Johnny Doe"
+  state           = "active"
+  department      = "Development"
+  title           = "Senior Director"
+  manager         = Rajesh
+  acd_auto_answer = true
+  profile_skills  = ["Java", "Go"]
+  certifications  = ["Certified Developer"]
+  addresses {
+    other_emails {
+      address = "john@gmail.com"
+      type    = "HOME"
+    }
+    phone_numbers {
+      number     = "+13174181234"
+      media_type = "PHONE"
+      type       = "MOBILE"
+    }
+  }
+  
+  locations {
+    location_id = "45a09a4d-6d1f-4ead-b369-eb2e67533125"
+    notes       = "Office 201"
+  }
+  employer_info {
+    official_name = "Jonathon Doe"
+    employee_id   = "12345"
+    employee_type = "Full-time"
+    date_hire     = "2021-03-18"
+  }
+  routing_utilization {
+    call {
+      maximum_capacity = 1
+      include_non_acd  = true
+    }
+    callback {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "email"]
+    }
+    chat {
+      maximum_capacity          = 3
+      include_non_acd           = false
+      interruptible_media_types = ["call"]
+    }
+    email {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+    message {
+      maximum_capacity          = 4
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+  }
+}
+
+resource "genesyscloud_user" "example_user2" {
+  email = "bobby@servion.com"
+  name  = "Bobby Drop Tables"
+  title = "CEO"
+  state = "active"
+}
